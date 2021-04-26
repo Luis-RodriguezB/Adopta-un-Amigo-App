b04e0761704621c29b7f7e95727d0c6b3e694850
arlenvargas2000@gmail.com <arlenvargas2000@gmail.com>
2021-04-26 11:20:15 -0600

    agregar redux a la app

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import store from "./src/redux/store/store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app si!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>tomela xd!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/redux/reducers/ExampleReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  dogs: [],
+  error: null,
+  loading: false,
+};
+
+const DogsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default DogsReducer;
