19eb9b8346ef0da987a434ea042e29ee0629d42e
tatimoramen <tatimorales02@icloud.com>
2021-05-11 21:46:50 -0600

    pagina principla

--- src/view/screen/homeScreen/index.js
+++ src/view/screen/homeScreen/index.js
@@ -1,18 +1,43 @@
 import React from "react";
-import { Text, View } from "native-base";
+import {
+  ScrollView,
+  Text,
+  View,
+  ImageBackground,
+  StyleSheet,
+  SafeAreaView
+} from "react-native";
+import perritos from "../../../../assets/imag.jpg";
 
 const Home = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{fontFamily: 'Salsa-Regular'}} >Inicio</Text>
-    </View>
+    <SafeAreaView style={{flex:1}}>
+    <ScrollView style={{ flexGrow:1, height:"100%"}}>
+      <View style={style.container}>
+        <ImageBackground
+          source={perritos}
+          style={{ flex: 3, resizeMode: "stretch" }}
+        >
+          <Text
+            style={{
+              fontFamily: "Salsa-Regular",
+              textAlign: "center",
+              fontSize: 30,
+            }}
+          >
+            Inicio
+          </Text>
+        </ImageBackground>
+        <View style={{ flex: 1 }}>
+          <Text>Tati</Text>
+        </View>
+      </View>
+    </ScrollView>
+    </SafeAreaView>
   );
 };
+const style = StyleSheet.create({
+  container: { flex: 1, flexDirection: "column" },
+});
 
 export default Home;
