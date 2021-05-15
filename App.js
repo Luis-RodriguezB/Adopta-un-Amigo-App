6c6ee2cc24ca0dde796cd9c36c97d76ff4fb5da9
Luis <luis.rodriguez.baltodano@est.una.ac.cr>
2021-05-15 16:45:40 -0600

    ..

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./src/redux/store/store";
-import AppNavigator from './src/navigation/index';
-import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from "./src/navigation/index";
+import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
 
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <AppNavigator/>
+        <View style={{flex: 1, paddingTop: 40}}>
+          <AppNavigator />
+        </View>
       </NavigationContainer>
     </Provider>
   );
-}
- 
+};
+
 export default App;
--- src/view/screen/homeScreen/index.js
+++ src/view/screen/homeScreen/index.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { Text, View, ImageBackground, StyleSheet } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import {
+  Text,
+  View,
+  ImageBackground,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 import perritos from "../../../../assets/imag.jpg";
 import information from "./components/information";
 import Contact from "./components/contacts";
@@ -8,38 +13,38 @@ import Contact from "./components/contacts";
 const Home = () => {
   console.log(information);
   return (
-    <View style={style.container}>
-      <View style={{ flex: 3, margin: 0 }}>
-        <ImageBackground source={perritos} style={style.image}>
-          <Text style={style.tittle}>Adopta un amigo</Text>
-        </ImageBackground>
-        <Text
-          style={{
-            margin: 5,
-            paddingRight: 10,
-            paddingLeft: 10,
-            paddingTop: 10,
-            fontSize: 16,
-          }}
-        >
-          Somos una organización sin fines de lucro que buscar rescatar y darles
-          un hogar a los perros de la calle en la zona de Liberia.
-        </Text>
-      </View>
-      <View style={style.textContainer}>
-        {information.map((item, index) => {
-          return (
-            <Text style={style.text} key={index}>
-              {item.name}: {item.description}
-            </Text>
-          );
-        })}
-      </View>
-    </View>
+      <ScrollView contentContainerStyle={{flex:1}}>
+        <View style={{ flex: 3, margin: 0 }}>
+          <ImageBackground source={perritos} style={style.image}>
+            <Text style={style.tittle}>Adopta un amigo</Text>
+          </ImageBackground>
+          <Text
+            style={{
+              margin: 5,
+              paddingRight: 10,
+              paddingLeft: 10,
+              paddingTop: 10,
+              fontSize: 16,
+            }}
+          >
+            Somos una organización sin fines de lucro que buscar rescatar y
+            darles un hogar a los perros de la calle en la zona de Liberia.
+          </Text>
+        </View>
+        <View style={style.textContainer}>
+          {information.map((item, index) => {
+            return (
+              <Text style={style.text} key={index}>
+                {item.name}: {item.description}
+              </Text>
+            );
+          })}
+        </View>
+      </ScrollView>
   );
 };
 const style = StyleSheet.create({
-  container: { flex: 1, paddingTop: 40, margin: 0 },
+  container: { flex: 1, margin: 0 },
   image: {
     flex: 1,
     resizeMode: "stretch",
