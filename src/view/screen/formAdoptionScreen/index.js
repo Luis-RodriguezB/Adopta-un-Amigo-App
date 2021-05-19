4fc973a05e063d43c9cda26ec695bd6d5eecbd5d
tatimoramen <tatimorales02@icloud.com>
2021-05-19 16:09:18 -0600

    formulario

--- src/view/screen/formAdoptionScreen/index.js
+++ src/view/screen/formAdoptionScreen/index.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { Text, View } from "native-base";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Text
+  } from "react-native";
 
 const FormAdoptionDog = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Adopcion</Text>
+    <View style={styles.tittle}>
+      <Text>Nombre</Text>
+      <View>
+      <TextInput placeholder="Nombre" />
+      <TextInput placeholder="Primer apellido" />
+      <TextInput placeholder="Segundo apellido" />
+      <TextInput placeholder="Teléfono" />
+      <TextInput placeholder="Correo eléctronico" />
+      <TextInput placeholder="Dirección" />
+    </View>
     </View>
   );
 };
 
+
 export default FormAdoptionDog;
+
+const styles = StyleSheet.create({
+  tittle: {
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#FFF",
+  },
+});
