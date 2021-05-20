07372b0888d4a98c63b468412025821cc23e36b1
tatimoramen <tatimorales02@icloud.com>
2021-05-20 08:22:44 -0600

    formulario

--- src/view/screen/formAdoptionScreen/index.js
+++ src/view/screen/formAdoptionScreen/index.js
@@ -3,33 +3,92 @@ import {
   View,
   StyleSheet,
   TextInput,
-  Text
-  } from "react-native";
+  Text,
+  ScrollView,
+  Button,
+} from "react-native";
 
 const FormAdoptionDog = () => {
   return (
-    <View style={styles.tittle}>
-      <Text>Nombre</Text>
-      <View>
-      <TextInput placeholder="Nombre" />
-      <TextInput placeholder="Primer apellido" />
-      <TextInput placeholder="Segundo apellido" />
-      <TextInput placeholder="Teléfono" />
-      <TextInput placeholder="Correo eléctronico" />
-      <TextInput placeholder="Dirección" />
-    </View>
-    </View>
+    <ScrollView contentContainerStyle={{ flex: 1 }}>
+      <Text style={styles.header}>Formulario de Adopción</Text>
+      <View style={styles.subtittle}>
+      <Text>Para continuar con el proceso de adopción, debes llenar el formulario</Text>
+      </View>
+      <View style={styles.tittle}>
+        <View>
+          <Text>Nombre</Text>
+          <TextInput style={styles.input} placeholder="Escribe tu nombre" />
+          <Text>Primer apellido</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Escribe tu primer apellido"
+          />
+          <Text>Segundo apellido</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Escribe tu segundo apellido"
+          />
+          <Text>Teléfono</Text>
+          <TextInput style={styles.input} placeholder="Escribe tu Teléfono" />
+          <Text>Correo eléctronico</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Escribe tu correo eléctronico"
+          />
+          <Text>Dirección</Text>
+          <TextInput style={styles.input} placeholder="Escribe tu dirección" />
+          <Text>Descripción</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Escribe una breve descripción"
+          />
+        </View>
+        <View style={styles.contentButton}>
+          <Button
+            style={{
+              color: "#fff",
+            }}
+          >
+            <Text>Enviar</Text>
+          </Button>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
-
 
 export default FormAdoptionDog;
 
 const styles = StyleSheet.create({
   tittle: {
-    fontSize: 30,
-    textAlign: "center",
+    margin: 10,
+    height: 75,
+    paddingLeft: 10,
     fontWeight: "bold",
-    color: "#FFF",
+  },
+  input: {
+    borderColor: "lightgrey",
+    margin: 10,
+    height: 50,
+    paddingLeft: 10,
+    borderWidth: 1,
+  },
+  contentButton: {
+    backgroundColor: "green",
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    borderRadius: 30
+  },
+  header: {
+    margin: 15,
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "700",
+  },
+  subtittle: {
+    margin: 15,
+    textAlign: "left",
+    fontSize: 18,
   },
 });
